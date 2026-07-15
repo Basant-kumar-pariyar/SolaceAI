5498448c9df1ab9aca7ed5206cd3dd29e62a923e
basant <basantkumar97248@gmail.com>
2026-07-15 21:54:30 +0530

    Complete authentication module with JWT

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -7,8 +7,24 @@ import {
     register,
     login,
 } from "../controllers/auth.controller.js";
+import { authenticateUser } from "../middleware/authJwt.middleware.js";
 
 const router = express.Router();
 router.post("/register",validateRegister,register);
 router.post("/login", validateLogin, login);
+router.get(
+    "/me",
+    authenticateUser,
+    (req, res) => {
+
+        res.json({
+
+            success:true,
+
+            user:req.user
+
+        });
+
+    }
+);
 export default router;
