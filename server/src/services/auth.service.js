bd22b768d8012a1cc8da7bd4187d8134743b07b6
basant <basantkumar97248@gmail.com>
2026-07-15 20:57:03 +0530

    Implement secure user registration with bcrypt

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,12 +1,33 @@
+import bcrypt from "bcrypt";
 import User from "../models/User.js";
 
 export const registerUser = async (userData) => {
+
+    const existingUser = await User.findOne({
+        email: userData.email,
+    });
+
+    if (existingUser) {
+        throw new Error("Email already registered.");
+    }
+
+    const hashedPassword = await bcrypt.hash(
+        userData.password,
+        10
+    );
+
+    userData.password = hashedPassword;
 
     const user = await User.create(userData);
 
     return {
         success: true,
         message: "User registered successfully.",
-        data: user,
+        data: {
+            id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            role: user.role,
+        },
     };
 };
