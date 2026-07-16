4c7107311d334101566461c228f4d056280bbc66
basant <basantkumar97248@gmail.com>
2026-07-16 07:33:37 +0530

    Complete Mood CRUD module. wallla!

--- server/src/controllers/mood.controller.js
+++ server/src/controllers/mood.controller.js
@@ -1,5 +1,5 @@
 import Mood from "../models/Mood.js";
-
+// create mood 
 export const createMood = async (req, res) => {
   try {
     const { mood, intensity, note } = req.body;
@@ -22,4 +22,104 @@ export const createMood = async (req, res) => {
       message: error.message,
     });
   }
+};
+// get mood history
+export const getMoodHistory = async (req, res) => {
+  try {
+
+    const moods = await Mood.find({
+      userId: req.user.userId,
+    }).sort({
+      createdAt: -1,
+    });
+
+    res.status(200).json({
+      success: true,
+      moods,
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+
+  }
+};
+// we can update mood via user --- created mee haha
+export const updateMood = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+
+        const { mood, intensity, note } = req.body;
+
+        const updatedMood = await Mood.findOneAndUpdate(
+            {
+                _id: id,
+                userId: req.user.userId,
+            },
+            {
+                mood,
+                intensity,
+                note,
+            },
+            {
+                new: true,
+            }
+        );
+
+        if (!updatedMood) {
+            return res.status(404).json({
+                success: false,
+                message: "Mood not found.",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            mood: updatedMood,
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+
+    }
+};
+// delete mood .... history
+export const deleteMood = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+
+        const deletedMood = await Mood.findOneAndDelete({
+            _id: id,
+            userId: req.user.userId,
+        });
+
+        if (!deletedMood) {
+            return res.status(404).json({
+                success: false,
+                message: "Mood not found.",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Mood deleted successfully.",
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+
+    }
 };
--- server/src/routes/mood.routes.js
+++ server/src/routes/mood.routes.js
@@ -1,9 +1,29 @@
 import express from "express";
 import { authenticateUser } from "../middleware/authJwt.middleware.js";
-import { createMood } from "../controllers/mood.controller.js";
-
+import {
+    createMood,
+    getMoodHistory,
+    updateMood,
+    deleteMood,
+} from "../controllers/mood.controller.js";
 const router = express.Router();
 
 router.post("/", authenticateUser, createMood);
+router.get(
+  "/",
+  authenticateUser,
+  getMoodHistory
+);
+router.put(
+    "/:id",
+    authenticateUser,
+    updateMood
+);
+
+router.delete(
+    "/:id",
+    authenticateUser,
+    deleteMood
+);
 
 export default router;
